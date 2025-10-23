cd0f040709e4ed069b61336fb8360523622afc6a
DanRoxs <danroxsll@gmail.com>
2025-10-23 12:01:42 -0600

    Slider Animation JS

--- Proyectos/Startup/src/js/slideshow.js
+++ Proyectos/Startup/src/js/slideshow.js
@@ -2,6 +2,7 @@ const slideshow = document.getElementById('slideshow');
 const slides = document.getElementById('slides')
 const indicadores = document.getElementById('indicadores')
 const btnCambiar = document.getElementById('cambiar')
+let slideActual = 1
 
 const siguienteSlide = () => {
     const primerSlide = slides.children[0];
@@ -16,6 +17,16 @@ const siguienteSlide = () => {
         slides.style.transition = 'none';
         slides.style.transform = `translateX(0)`
     }, velocidad);
+
+    // TODO - Cambio de indicador
+    if (slideActual < slides.children.length) {
+        slideActual++;
+    } else {
+        slideActual = 1;
+    }
+    indicadores.querySelector('.active').classList.remove('active')
+    indicadores.children[slideActual - 1].classList.add('active')
+
 }
 
 setInterval(() => {
