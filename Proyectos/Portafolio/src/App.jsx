4cd9afd7e82b3b403a7b148dde9bd55dd466b11c
DanRoxs <danroxsll@gmail.com>
2025-10-31 11:40:27 -0600

    Estructura Header Responsive

---
+++ Proyectos/Portafolio/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./componentes/layout/Header"
+
+function App () {
+    return (
+        <>
+            <div className="contenedor">
+            <Header />
+            </div>
+        </>
+    )
+}
+
+export default App
---
+++ Proyectos/Portafolio/src/componentes/layout/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css'
+
+const Header = () => {
+    return (
+        <>
+        <header className="header">
+             <div className="logo">
+                <h2 className="titulo">Dan Rox</h2>
+                <p className="subtitulo">Desarrollador Web & Freelance</p>
+             </div>
+             <nav className="navbar">
+                <a href="#trabajos">Trabajos</a>
+                <a href="#acerca-de">Acerca de</a>
+                <a href="#contacto">Contacto</a>
+             </nav>
+             <button>Dark mode</button>
+        </header>
+        </>
+    )
+}
+
+export default Header
